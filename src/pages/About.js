bd96bbf558fad493dd57399d730149be05a077a8
MalicknND <ndiayemalick062@gmail.com>
2023-11-01 16:44:49 +0100

    countries

---
+++ src/pages/About.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A Propos</h1>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+        aliquid itaque blanditiis dolorem. Iste assumenda est beatae amet illo
+        fuga dignissimos debitis at, obcaecati odit tempora dicta fugiat dolorum
+        reprehenderit sunt eius vero magni, sit distinctio voluptatem? Sunt
+        laudantium quae adipisci error reprehenderit, culpa maiores aut, dolore
+        quod placeat unde totam ut consequatur saepe obcaecati. Ipsa ad laborum
+        error autem. Eum ad animi quas, iusto accusantium commodi deleniti a
+        dolorum, dolor nam optio reiciendis quis recusandae obcaecati sapiente,
+        eligendi nulla suscipit error tenetur nobis inventore aut ducimus
+        provident consectetur? Odio recusandae maxime id a porro nemo
+        consequuntur culpa deserunt eum.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, totam.
+        Assumenda repudiandae autem modi earum tempore at, consectetur harum sed
+        qui minima ex voluptatem error! Placeat, quam fugit magnam nam quo
+        aliquam ratione aspernatur quas illum inventore adipisci vitae qui
+        minima! Aut molestias minima ullam cum voluptatibus recusandae et
+        delectus labore aliquid, adipisci rem. Rerum fugiat cumque amet quia
+        voluptatem deleniti perferendis eligendi vel expedita. Fugit veniam
+        nihil maxime provident, voluptate consectetur veritatis ab unde libero
+        minus obcaecati enim perferendis cupiditate soluta sunt inventore
+        quaerat porro. Modi praesentium totam suscipit dicta nesciunt vero eos
+        ab aut repellat inventore. Harum quae nesciunt nostrum quia illo
+        cupiditate doloremque minima voluptas explicabo? Nisi ipsa a debitis
+        exercitationem nostrum vitae, facilis sint reprehenderit reiciendis
+        quibusdam adipisci amet at. Doloribus, beatae! Magnam reiciendis nemo,
+        quidem ipsa nobis amet dolorem maiores, alias veritatis, cumque optio
+        impedit inventore vel! Quod repellendus labore non facere est? Dolores
+        necessitatibus officiis iste harum vel! A culpa obcaecati eum omnis
+        libero architecto soluta aspernatur corrupti dolorum veniam adipisci
+        dolorem nisi iste quod hic facere dolores ullam ipsam, odio velit! Autem
+        sunt animi fuga reiciendis minima quod quisquam facere dolor, optio
+        rerum alias quidem qui culpa corporis! Possimus sunt minus sint itaque!
+      </p>
+    </div>
+  );
+};
+
+export default About;
